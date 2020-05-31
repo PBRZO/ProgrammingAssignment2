a76be10dc1c547d2769a359ad42ca5aacaf38cb7
PBRZO <pius_brzoska@yahoo.com>
2020-05-31 11:52:33 -0700

    cacheMatrix commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  #makeCacheMatrix takes x as a matrix
+  #global inverse matrix im initialized to NULL
+  im <<- NULL
+  set <- function(y) {
+    #overwrite global x with y
+    x <<- y
+    #initialize global im to NULL
+    im <<- NULL
+  }
+  get <-function() x #return x
+  getIm <- function() im #return im
+  setIm <- function(inverseM) im <<- inverseM #set im to the inverse matrix
+  list(set = set, get = get,
+       getIm=getIm,
+       setIm = setIm
+       )
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #check whether im is not NULL, a) get it b) if not null return inverse matrix
+  im <- x$getIm()
+  if (!is.null(im)) {
+    message("get inverse matrix ")
+    return(im)
+  }
+  #below is executed if im is NULL
+  #a) get the matrix of the object x
+  im_data <- x$get()
+  #generate the reverse matrix
+  im <- solve(im_data,...)
+  #store the reverse matrix in the object x
+  x$setIm(im)
+  #return inverse matrix
+  im
 }
+
